a82553a9f5e8b72d45a7100835f83c968e4445b6
Pablo Fernandez Michel <pfdzm@users.noreply.github.com>
2019-11-17 19:15:28 +0100

    fetch public profile info using GH rest API

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,29 @@
+let inquirer = require("inquirer");
+const Octokit = require("@octokit/rest");
+
+// https://octokit.github.io/rest.js/#usage
+// User agent is required
+
+const octokit = Octokit({
+  userAgent: "portfolio-generator v0.1"
+});
+
 const questions = [
-  
+  { type: "input", name: "user", message: "GitHub username:" }
 ];
 
-function writeToFile(fileName, data) {
- 
-}
+function writeToFile(fileName, data) {}
 
 function init() {
+  inquirer.prompt(questions).then(async answers => {
+    let username = answers.user;
+
+    let { data } = await octokit.users.getByUsername({
+      username
+    });
+
+    console.log(data);
+  });
+}
 
 init();
