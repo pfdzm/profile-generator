95706c1129c01c501d40e212a959d4b56de69c23
Pablo Fernandez Michel <pfdzm@users.noreply.github.com>
2019-11-17 19:34:02 +0100

    generate html file based on user input

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,7 @@
-let inquirer = require("inquirer");
+const inquirer = require("inquirer");
 const Octokit = require("@octokit/rest");
+const generator = require("./generateHTML.js");
+const fs = require("fs");
 
 // https://octokit.github.io/rest.js/#usage
 // User agent is required
@@ -12,7 +14,13 @@ const questions = [
   { type: "input", name: "user", message: "GitHub username:" }
 ];
 
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  let html = generator.generateHTML(data);
+  fs.writeFile(`./${fileName}`, html, function(err) {
+    if (err) throw err;
+    console.log("HTML generated!");
+  });
+}
 
 function init() {
   inquirer.prompt(questions).then(async answers => {
@@ -21,6 +29,8 @@ function init() {
     let { data } = await octokit.users.getByUsername({
       username
     });
+
+    writeToFile("index.html", { color: "red", user: data });
 
     console.log(data);
   });
